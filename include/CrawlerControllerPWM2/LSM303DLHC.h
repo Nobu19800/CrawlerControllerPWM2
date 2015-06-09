7b1ebf1ca55045f78a1a91c405c353b42fe01be0
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2015-06-09 16:31:44 +0900

    1631

---
+++ include/CrawlerControllerPWM2/LSM303DLHC.h
@@ -0,0 +1,218 @@
+#ifndef LSM303DLHC_H
+#define LSM303DLHC_H
+
+#include <stdio.h>
+#include <mraa.hpp>
+
+
+#include "i2c_smf.h"
+
+
+ 
+
+class LSM303DLHC
+{
+public:
+	LSM303DLHC(mraa::I2c *i2c, i2c_smf *smf, uint8_t Accaddr = 0x19, uint8_t Magnaddr = 0x1e, double ar = 0.2, double mr = 0.2, int mx_offset = 935, int my_offset = 320, int mz_offset = 730);
+	~LSM303DLHC();
+	
+	void reset(void);
+	void getAcc(double &ax, double &ay, double &az);
+	void getMagn(double &mx, double &my, double &mz);
+	double getTemp();
+
+	void getAccData(double &ax, double &ay, double &az);
+	void getMagnData(double &mx, double &my, double &mz);
+	double getTempData();
+	
+	void getOrientation(double &rx, double &ry, double &rz);
+
+	void setAccScale(uint8_t scale);
+	
+	void setMagnScale(uint8_t scale);
+	
+	enum regAddr
+	{
+	      TEMP_OUT_L        = 0x05,
+	      TEMP_OUT_H        = 0x06,
+
+	      STATUS_M          = 0x07,
+
+	      INT_CTRL_M        = 0x12,
+	      INT_SRC_M         = 0x13,
+	      INT_THS_L_M       = 0x14,
+	      INT_THS_H_M       = 0x15,
+
+	      OFFSET_X_L_M      = 0x16,
+	      OFFSET_X_H_M      = 0x17,
+	      OFFSET_Y_L_M      = 0x18,
+	      OFFSET_Y_H_M      = 0x19,
+	      OFFSET_Z_L_M      = 0x1A,
+	      OFFSET_Z_H_M      = 0x1B,
+	      REFERENCE_X       = 0x1C,
+	      REFERENCE_Y       = 0x1D,
+	      REFERENCE_Z       = 0x1E,
+
+	      CTRL0             = 0x1F,
+	      CTRL1             = 0x20,
+	      CTRL_REG1_A       = 0x20,
+	      CTRL2             = 0x21,
+	      CTRL_REG2_A       = 0x21,
+	      CTRL3             = 0x22,
+	      CTRL_REG3_A       = 0x22,
+	      CTRL4             = 0x23,
+	      CTRL_REG4_A       = 0x23,
+	      CTRL5             = 0x24,
+	      CTRL_REG5_A       = 0x24,
+	      CTRL6             = 0x25,
+	      CTRL_REG6_A       = 0x25,
+	      HP_FILTER_RESET_A = 0x25,
+	      CTRL7             = 0x26,
+	      REFERENCE_A       = 0x26,
+	      STATUS_A          = 0x27,
+	      STATUS_REG_A      = 0x27,
+
+	      OUT_X_L_A         = 0x28,
+	      OUT_X_H_A         = 0x29,
+	      OUT_Y_L_A         = 0x2A,
+	      OUT_Y_H_A         = 0x2B,
+	      OUT_Z_L_A         = 0x2C,
+	      OUT_Z_H_A         = 0x2D,
+
+	      FIFO_CTRL         = 0x2E,
+	      FIFO_CTRL_REG_A   = 0x2E,
+	      FIFO_SRC          = 0x2F,
+	      FIFO_SRC_REG_A    = 0x2F,
+
+	      IG_CFG1           = 0x30,
+	      INT1_CFG_A        = 0x30,
+	      IG_SRC1           = 0x31,
+	      INT1_SRC_A        = 0x31,
+	      IG_THS1           = 0x32,
+	      INT1_THS_A        = 0x32,
+	      IG_DUR1           = 0x33,
+	      INT1_DURATION_A   = 0x33,
+	      IG_CFG2           = 0x34,
+	      INT2_CFG_A        = 0x34,
+	      IG_SRC2           = 0x35,
+	      INT2_SRC_A        = 0x35,
+	      IG_THS2           = 0x36,
+	      INT2_THS_A        = 0x36,
+	      IG_DUR2           = 0x37,
+	      INT2_DURATION_A   = 0x37,
+
+	      CLICK_CFG         = 0x38,
+	      CLICK_CFG_A       = 0x38,
+	      CLICK_SRC         = 0x39,
+	      CLICK_SRC_A       = 0x39,
+	      CLICK_THS         = 0x3A,
+	      CLICK_THS_A       = 0x3A,
+	      TIME_LIMIT        = 0x3B,
+	      TIME_LIMIT_A      = 0x3B,
+	      TIME_LATENCY      = 0x3C,
+	      TIME_LATENCY_A    = 0x3C,
+	      TIME_WINDOW       = 0x3D,
+	      TIME_WINDOW_A     = 0x3D,
+
+	      Act_THS           = 0x3E,
+	      Act_DUR           = 0x3F,
+
+	      CRA_REG_M         = 0x00,
+	      CRB_REG_M         = 0x01,
+	      MR_REG_M          = 0x02,
+
+	      SR_REG_M          = 0x09,
+	      IRA_REG_M         = 0x0A,
+	      IRB_REG_M         = 0x0B,
+	      IRC_REG_M         = 0x0C,
+
+	      WHO_AM_I          = 0x0F,
+	      WHO_AM_I_M        = 0x0F,
+
+	      TEMP_OUT_H_M      = 0x31,
+	      TEMP_OUT_L_M      = 0x32,
+
+
+
+	      OUT_X_H_M         = -1,
+	      OUT_X_L_M         = -2,
+	      OUT_Y_H_M         = -3,
+	      OUT_Y_L_M         = -4,
+	      OUT_Z_H_M         = -5,
+	      OUT_Z_L_M         = -6,
+
+
+	      DLH_OUT_X_H_M     = 0x03,
+	      DLH_OUT_X_L_M     = 0x04,
+	      DLH_OUT_Y_H_M     = 0x05,
+	      DLH_OUT_Y_L_M     = 0x06,
+	      DLH_OUT_Z_H_M     = 0x07,
+	      DLH_OUT_Z_L_M     = 0x08,
+
+	      DLM_OUT_X_H_M     = 0x03,
+	      DLM_OUT_X_L_M     = 0x04,
+	      DLM_OUT_Z_H_M     = 0x05,
+	      DLM_OUT_Z_L_M     = 0x06,
+	      DLM_OUT_Y_H_M     = 0x07,
+	      DLM_OUT_Y_L_M     = 0x08,
+
+	      DLHC_OUT_X_H_M    = 0x03,
+	      DLHC_OUT_X_L_M    = 0x04,
+	      DLHC_OUT_Z_H_M    = 0x05,
+	      DLHC_OUT_Z_L_M    = 0x06,
+	      DLHC_OUT_Y_H_M    = 0x07,
+	      DLHC_OUT_Y_L_M    = 0x08,
+
+	      D_OUT_X_L_M       = 0x08,
+	      D_OUT_X_H_M       = 0x09,
+	      D_OUT_Y_L_M       = 0x0A,
+	      D_OUT_Y_H_M       = 0x0B,
+	      D_OUT_Z_L_M       = 0x0C,
+	      D_OUT_Z_H_M       = 0x0D
+	};
+
+	enum Ascale {
+	  AFS_2G = 0,
+	  AFS_4G,
+	  AFS_8G,
+	  AFS_16G
+	};
+
+	enum Mscale {
+	  MFS_1300mG = 0,
+	  MFS_1900mG,
+	  MFS_2500mG,
+	  MFS_4000mG,
+	  MFS_4700mG,
+	  MFS_5600mG,
+	  MFS_8100mG,
+	};
+private:
+	
+	//int _i2channel;
+
+	void readByte(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data);
+	void writeByte(uint8_t Address, uint8_t Register, uint8_t Data);
+	
+	uint8_t _Accaddr;
+	uint8_t _Magnaddr;
+	mraa::I2c *_i2c;
+	double _ar, _mr;
+	double lastAX, lastAY, lastAZ;
+	double lastMX, lastMY, lastMZ;
+	
+	double lastTemp;
+	
+	uint8_t _Ascale;
+	uint8_t _Mscale;
+
+	i2c_smf *_smf;
+
+	int _mx_offset, _my_offset, _mz_offset;
+};
+
+
+
+
+
+#endif
---
+++ src/LSM303DLHC.cpp
@@ -0,0 +1,293 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <mraa.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+#include "LSM303DLHC.h"
+
+LSM303DLHC::LSM303DLHC(mraa::I2c *i2c, i2c_smf *smf, uint8_t Accaddr, uint8_t Magnaddr, double ar, double mr, int mx_offset, int my_offset, int mz_offset) {
+	_i2c = i2c;
+	
+	
+	_ar = ar;
+	_mr = mr;
+	
+	
+	_Accaddr = Accaddr;
+	_Magnaddr = Magnaddr;
+
+	//_smf = new i2c_smf();
+	_smf = smf;
+
+	_mx_offset = mx_offset;
+	_my_offset = my_offset;
+	_mz_offset = mz_offset;
+
+	reset();
+}
+
+LSM303DLHC::~LSM303DLHC() {
+	//delete _smf;
+}
+
+
+
+void LSM303DLHC::setAccScale(uint8_t scale)
+{
+	_Ascale = scale;
+	writeByte(_Accaddr, CTRL_REG4_A, _Ascale << 3);
+	
+	
+}
+
+
+
+void LSM303DLHC::setMagnScale(uint8_t scale){
+	_Mscale = scale;
+	writeByte(_Magnaddr, CRB_REG_M, _Mscale << 3);
+	
+}
+
+void LSM303DLHC::reset(void) {
+	
+	
+	
+
+	
+	//_i2c->address(_Accaddr);
+
+	
+	writeByte(_Accaddr,CTRL_REG1_A,0x27);
+
+	struct timespec ts;
+	ts.tv_sec = 0;
+	ts.tv_nsec = 200000000;
+	nanosleep(&ts, NULL);
+	
+	setAccScale(AFS_16G);
+	
+	
+	
+	writeByte(_Magnaddr,CRA_REG_M,0x14);
+	nanosleep(&ts, NULL);
+	//writeByte(_Magnaddr,CRB_REG_M,0x20);
+	writeByte(_Magnaddr,MR_REG_M,0x00);
+	nanosleep(&ts, NULL);
+	setMagnScale(MFS_2500mG);
+
+
+	nanosleep(&ts, NULL);
+	
+  	
+  	
+  	
+ 
+  	
+  	
+
+	const double count = 10;
+	double ax,ay,az;
+	lastAX = 0;
+	lastAY = 0;
+	lastAZ = 0;
+	
+	for(int i=0;i < count;i++)
+	{
+		getAccData(ax,ay,az);
+
+		lastAX += ax/count;
+		lastAY += ay/count;
+		lastAZ += az/count;
+	}
+
+
+	
+
+	double mx,my,mz;
+	
+	lastMX = 0;
+	lastMY = 0;
+	lastMZ = 0;
+	for(int i=0;i < count;i++)
+	{
+		getMagnData(mx,my,mz);
+		lastMX += mx/count;
+		lastMY += my/count;
+		lastMZ += mz/count;
+	}
+
+	
+}
+
+void LSM303DLHC::getOrientation(double &rx, double &ry, double &rz)
+{
+	
+
+	ry = atan2(lastAY,lastAZ);
+	rx = atan2(-lastAX,sqrt(lastAY*lastAY+lastAZ*lastAZ));
+
+	
+
+	double mxi = cos(ry)*lastMX + sin(rx)*sin(ry)*lastMY + cos(rx)*sin(ry)*lastMZ;
+	double myi = cos(rx)*lastMY - sin(rx)*lastMZ;
+
+	
+
+	rz = atan2(-myi,mxi);
+
+	//std::cout << rz << "\t" << lastMX << "\t" << lastMY << "\t" << lastMZ << std::endl;
+}
+
+
+void LSM303DLHC::getAcc(double &ax, double &ay, double &az)
+{
+	getAccData(ax,ay,az);
+
+	lastAX = _ar*ax + (1-_ar)*lastAX;
+	lastAY = _ar*ay + (1-_ar)*lastAY;
+	lastAZ = _ar*az + (1-_ar)*lastAZ;
+	
+
+}
+
+void LSM303DLHC::getMagn(double &mx, double &my, double &mz)
+{
+	getMagnData(mx,my,mz);
+	//mx = _mr*mx + (1-_mr)*lastMX;
+	//my = _mr*my + (1-_mr)*lastMY;
+	//mz = _mr*mz + (1-_mr)*lastMZ;
+
+	lastMX = _mr*mx + (1-_mr)*lastMX;
+	lastMY = _mr*my + (1-_mr)*lastMY;
+	lastMZ = _mr*mz + (1-_mr)*lastMZ;
+
+	//std::cout << mx << "\t" << my << "\t" << mz << std::endl;
+}
+
+
+double LSM303DLHC::getTemp()
+{
+	lastTemp = getTempData();
+	return lastTemp;
+}
+
+
+
+
+
+void LSM303DLHC::getAccData(double &ax, double &ay, double &az) {
+	uint8_t Buf[6];
+	readByte(_Accaddr, OUT_X_L_A|0x80, 6, Buf);
+
+	
+
+	short AccelRaw_x = ((Buf[1] << 8) | Buf[0]);
+	short AccelRaw_y = ((Buf[3] << 8) | Buf[2]);
+	short AccelRaw_z = ((Buf[5] << 8) | Buf[4]);
+
+	double aRes;
+	if(_Ascale == AFS_2G)
+		aRes = 4.0/32768.0;
+	else if(_Ascale == AFS_4G)
+		aRes = 8.0/32768.0;
+	else if(_Ascale == AFS_8G)
+		aRes = 16.0/32768.0;
+	else
+		aRes = 32.0/32768.0;
+	ax = (double)AccelRaw_x * aRes;
+	ay = (double)AccelRaw_y * aRes;
+	az = (double)AccelRaw_z * aRes;
+
+	
+
+	
+}
+
+void LSM303DLHC::getMagnData(double &mx, double &my, double &mz) {
+	
+	
+
+	uint8_t Mag[6];
+
+	readByte(_Magnaddr, DLH_OUT_X_H_M, 6, Mag);
+
+	short MagnRaw_x = ((Mag[0] << 8) | Mag[1]);
+	short MagnRaw_y = ((Mag[4] << 8) | Mag[5]);
+	short MagnRaw_z = ((Mag[2] << 8) | Mag[3]);
+	
+	MagnRaw_x += _mx_offset;
+	MagnRaw_y += _my_offset;
+	MagnRaw_z += _mz_offset;
+
+	//std::cout << MagnRaw_x << "\t" << MagnRaw_y << "\t" << MagnRaw_z << std::endl;
+
+	double mRes;
+	if(_Mscale == MFS_1300mG)
+		mRes = 2.6/32768.0;
+	else if(_Mscale == MFS_1900mG)
+		mRes = 3.8/32768.0;
+	else if(_Mscale == MFS_2500mG)
+		mRes = 5.0/32768.0;
+	else if(_Mscale == MFS_4000mG)
+		mRes = 8.0/32768.0;
+	else if(_Mscale == MFS_4700mG)
+		mRes = 9.4/32768.0;
+	else if(_Mscale == MFS_5600mG)
+		mRes = 11.2/32768.0;
+	else
+		mRes = 16.2/32768.0;
+
+	mx = (double)MagnRaw_x * mRes;
+	my = (double)MagnRaw_y * mRes;
+	mz = (double)MagnRaw_z * mRes;
+
+	
+}
+
+double LSM303DLHC::getTempData()
+{
+	uint8_t Buf[2];
+	readByte(_Magnaddr,TEMP_OUT_H_M,1,Buf);
+
+	short Temp = Buf[0] << 8;
+
+	readByte(_Magnaddr,TEMP_OUT_L_M,1,Buf);
+
+	Temp = Temp | Buf[0];
+
+
+	
+	
+	
+	
+	return (double)Temp/8.;
+}
+
+
+void LSM303DLHC::writeByte(uint8_t Address, uint8_t Register, uint8_t Data) {
+
+	_smf->sem_lock();
+	_i2c->address(Address);
+	
+	_i2c->writeReg(Register, Data);
+	
+	_smf->sem_unlock();
+
+  	
+}
+
+void LSM303DLHC::readByte(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data) {
+	
+	_smf->sem_lock();
+
+	_i2c->address(Address);
+	_i2c->writeByte(Register);
+	
+	
+	_i2c->read(Data,Nbytes);
+	
+	_smf->sem_unlock();
+}
